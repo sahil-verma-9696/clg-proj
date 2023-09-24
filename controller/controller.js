71fac0fa166f8ac0b048cdfb9577ab1ebf8b2395
sahil-verma-9696 <laptopsahil123@gmail.com>
2023-09-24 06:53:22 +0530

    sendingn client data to data base

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,8 @@
+const userModel = require("./../userModel/userMode");
+
 const registration = async (req,res)=>{
+    const user = new userModel(req.body);
+    await user.save();
     console.log(req.body);
     res.json(req?.body)
 }
