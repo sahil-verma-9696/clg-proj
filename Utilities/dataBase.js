8d8f03ac813bcf5c92829b7cef249c2931d8166d
sahil-verma-9696 <laptopsahil123@gmail.com>
2023-10-07 14:13:30 +0530

    using cluster

--- Utilities/dataBase.js
+++ Utilities/dataBase.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
+const env = require("dotenv").config();
 
 const DataBase = (collection) => {
     try {
-        const uri = `mongodb://127.0.0.1:27017/${collection}`;
-        mongoose.connect(uri)
+        const pass =  process.env.MONGO_PASS;
+        const uri = `mongodb+srv://laptopsahil123:${pass}@cluster0.asnbdyu.mongodb.net/${collection}?retryWrites=true&w=majority&appName=AtlasApp`
+        mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+        // const uri = `mongodb://127.0.0.1:27017/${collection}`;
+        // mongoose.connect(uri)
     } catch (error) {
         console.log(error)
     }
