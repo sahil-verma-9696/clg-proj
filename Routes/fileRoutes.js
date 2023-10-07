014a02896bcdc28187ebedc5e45ca06cd065dd24
sahil-verma-9696 <laptopsahil123@gmail.com>
2023-10-07 16:05:33 +0530

    web site deploye but it has some problem with mongoos

--- Routes/fileRoutes.js
+++ Routes/fileRoutes.js
@@ -4,7 +4,7 @@ const controle = require("../Controllers/fileControler");
 
 const route = express.Router();
 
-route.use("/", controle.serveHome);
+route.use("/home", controle.serveHome);
 route.use("/login", (req, res, next) => { req.cookies.user ? res.redirect("/categories") : next() }, controle.serveLogin);
 route.use("/comp_faculty", (req, res, next) => { !req.cookies.user ? res.redirect("/login") : next() },controle.serveCompForm_faculty);
 route.use("/categories",controle.serveCategories)
