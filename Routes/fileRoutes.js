e7d23873a572a8a1d43e742f39d6f5badbf8f806
sahil-verma-9696 <laptopsahil123@gmail.com>
2023-10-08 09:38:50 +0530

    there is a problem in login render so i try to solve

--- Routes/fileRoutes.js
+++ Routes/fileRoutes.js
@@ -5,9 +5,10 @@ const controle = require("../Controllers/fileControler");
 const route = express.Router();
 
 route.use("/home", controle.serveHome);
-route.use("/login", (req, res, next) => { req.cookies.user ? res.redirect("/categories") : next() }, controle.serveLogin);
+route.use("/login", controle.serveLogin);
+// route.use("/login", (req, res, next) => { req.cookies.user ? res.redirect("/categories") : next() }, controle.serveLogin);
 route.use("/comp_faculty", (req, res, next) => { !req.cookies.user ? res.redirect("/login") : next() }, controle.serveCompForm_faculty);
-route.use("/categories", controle.serveCategories)
+route.use("/categories", controle.serveCategories); 
 
 
 module.exports = route
